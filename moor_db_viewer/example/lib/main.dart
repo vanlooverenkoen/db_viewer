e6616da20d232a4f41bb183cbe6adb70e287599f
Koen Van Looveren <vanlooverenkoen.dev@gmail.com>
2021-10-18 13:46:58 +0200

    Moved to an other structure

--- moor_db_viewer/example/lib/main.dart
+++ moor_db_viewer/example/lib/main.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:moor_db_viewer_example/my_app.dart';
+import 'package:moor_db_viewer_example/app.dart';
 
 void main() => runApp(MyApp());
---
+++ tool/packages_get.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd db_viewer
+flutter packages get
+
+cd moor_db_viewer
+flutter packages get
