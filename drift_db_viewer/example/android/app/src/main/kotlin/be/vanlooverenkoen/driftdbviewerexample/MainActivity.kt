bd11c8ca633f187a02463314ddecba12645a7cc2
Frank Vollebregt <fcjvollebregt@gmail.com>
2022-08-15 11:57:58 +0200

    Update drift and other dependencies and migrate Android Embedding in example

--- drift_db_viewer/example/android/app/src/main/kotlin/be/vanlooverenkoen/driftdbviewerexample/MainActivity.kt
+++ drift_db_viewer/example/android/app/src/main/kotlin/be/vanlooverenkoen/driftdbviewerexample/MainActivity.kt
@@ -3,7 +3,6 @@ package be.vanlooverenkoen.driftbviewerexample
 import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
